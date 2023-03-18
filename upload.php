a81139e6eebb66ddb0207eb509cd09d7f22ab18d
TinLe1310 <tintrungle1310@gmail.com>
2023-03-17 20:21:34 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,24 +12,22 @@
     //  Sanitize user input to escape HTML entities and filter out dangerous characters.
     $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
     $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $genre = filter_input(INPUT_POST, 'genre', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $rating = filter_input(INPUT_POST, 'rating', FILTER_VALIDATE_FLOAT);
 
-
-    if(empty($_POST['content']) || empty($_POST['title'])){
-        $message = "Your title and content must have at least 1 character !";
-    } 
-    elseif($_POST && !empty($_POST['title']) && !empty($_POST['genre']) 
-                        && !empty($_POST['author']) && !empty($_POST['content'])) {
-                //  Build the parameterized SQL query and bind to the above sanitized values.
-                $query = "INSERT INTO books (title, genre,) VALUES (:title, :content)";
-        }
-    
+    if($_POST){
+        //  Build the parameterized SQL query and bind to the above sanitized values.
+        $query = "INSERT INTO books (title, genre, author, content, rating) VALUES (:title, :genre, :author, :content, :rating)";  
         $statement = $db->prepare($query);
+
         //  Bind values to the parameters
         $statement->bindValue(":title", $title);
         $statement->bindValue(":genre", $genre);
         $statement->bindValue(":author", $author);
         $statement->bindValue(":content", $content);
+        $statement->bindValue(":rating", $rating);
 
         //  Execute the INSERT.
         //  execute() will check for possible SQL injection and remove if necessary
@@ -37,20 +35,15 @@
             header("Location: index.php");
             exit;
         }
+    }
+    else $message = " ❗❗❗ ERROR❗❗❗";
 
-        //  Execute the INSERT.
-        //  execute() will check for possible SQL injection and remove if necessary
-        if($statement->execute()){
-            header("Location: index.php");
-            exit;
-        }
 
 ?>
 <!DOCTYPE html>
 <html lang='en'>
 <head>
-    <title>My Blogs</title>
-    <link rel="stylesheet" type="text/css" href="main.css" />
+    <title>Error</title>
 </head>
 <body>
     <?php if(!empty($message)): ?>
